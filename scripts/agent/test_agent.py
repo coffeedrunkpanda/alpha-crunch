6efd8f94f76f6d012ede08c3d2343196ab86b306
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2026-03-10 20:13:34 +0100

    chore: update test agent to test rag_node

--- scripts/agent/test_agent.py
+++ scripts/agent/test_agent.py
@@ -1,4 +1,6 @@
 from alpha_crunch.agent.graph import alphaCrunch_agent
+from alpha_crunch.agent.state import AgentState
+from alpha_crunch.agent.rag_node import rag_node
 
 def run(query: str):
     # This initial state triggers the execution
@@ -17,9 +19,32 @@ def run(query: str):
     
     return result
 
+def test_rag(query: str):
+
+    # Mocking the LangGraph state
+    initial_state = {
+        "query": query,
+        "intent": "rag",
+        "retrieved_context": "",
+        "tool_output": None,
+        "final_answer": None,
+        "messages": []
+    }
+    # Run the node
+    new_state_data = rag_node(initial_state)
+    
+    print("\n=== THE FORMATTED CONTEXT SENT TO THE LLM ===")
+    print(new_state_data["retrieved_context"])
+
 if __name__ == "__main__":
     # result = run("What is an SEC 10-K filing?")
-    result = run("What was Apple's total revenue in Q3 2023?")
+    # result = run("What was Apple's total revenue in Q3 2023?")
+    # print(result["final_answer"])
 
+    result = test_rag("What are costco's main supply chain risk factors?")
     print("\n--- Final State ---")
-    print(result["final_answer"])
+    # print(result)
+
+
+
+    
