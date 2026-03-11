7cff5421b866dba75d949d96a9ee7ee8efa55088
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2026-03-11 17:20:55 +0100

    chore: update script with save company names functionality

--- scripts/rag/ingest_data.py
+++ scripts/rag/ingest_data.py
@@ -2,16 +2,17 @@ from pathlib import Path
 
 # Adjust this import based on your actual folder structure
 # Assuming this script is running from the root or a scripts folder
-from alpha_crunch.rag.ingest import build_vector_database
+from alpha_crunch.rag.ingest import build_vector_database, save_company_names_json
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+CHROMA_PATH = str(PROJECT_ROOT / "data" / "chroma_db")
+
+print(PROJECT_ROOT)
+print(CHROMA_PATH)
+
 
 def main():
     # Dynamically find the project root
-    PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-    CHROMA_PATH = str(PROJECT_ROOT / "data" / "chroma_db")
-
-    print(PROJECT_ROOT)
-    print(CHROMA_PATH)
-
     print(f"Targeting Database Path: {CHROMA_PATH}")
     
     # Run the ingestion function
@@ -20,6 +21,8 @@ def main():
         chroma_path=CHROMA_PATH, 
         max_rows=500, 
     )
+
+    save_company_names_json(CHROMA_PATH, 500)
 
 if __name__ == "__main__":
     main()
