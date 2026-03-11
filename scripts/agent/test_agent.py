0a6bf96158434760107d8f7bb0dc8690e6913839
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2026-03-11 17:27:56 +0100

    chore: clean up test script

--- scripts/agent/test_agent.py
+++ scripts/agent/test_agent.py
@@ -4,14 +4,7 @@ from alpha_crunch.agent.rag_node import rag_node
 
 def run(query: str):
     # This initial state triggers the execution
-    initial_state = {
-        "query": query,
-        "intent": None,
-        "retrieved_context": None,
-        "tool_output": None,
-        "final_answer": None,
-        "messages": []
-    }
+    initial_state = AgentState(query=query)
 
     print(f"Submitting query: {query}")
     # .invoke() is what actually runs the nodes and triggers the prints
@@ -19,32 +12,17 @@ def run(query: str):
     
     return result
 
-def test_rag(query: str):
-
-    # Mocking the LangGraph state
-    initial_state = {
-        "query": query,
-        "intent": "rag",
-        "retrieved_context": "",
-        "tool_output": None,
-        "final_answer": None,
-        "messages": []
-    }
-    # Run the node
-    new_state_data = rag_node(initial_state)
-    
-    print("\n=== THE FORMATTED CONTEXT SENT TO THE LLM ===")
-    print(new_state_data["retrieved_context"])
 
 if __name__ == "__main__":
+
     # result = run("What is an SEC 10-K filing?")
+    # result = run("What are Apple's main supply chain risks?")
+    # result = run("Did JPM beat earnings?")
     # result = run("What was Apple's total revenue in Q3 2023?")
-    # print(result["final_answer"])
+    result = run("What are costco's main supply chain risk factors?")
 
-    result = test_rag("What are costco's main supply chain risk factors?")
-    print("\n--- Final State ---")
-    # print(result)
+    # Print the final result
+    print("\n=== FINAL ANSWER ===")
+    print(result["final_answer"])
 
 
-
-    
