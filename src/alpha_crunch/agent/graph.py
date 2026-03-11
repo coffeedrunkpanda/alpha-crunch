e987d0447c3e7ac6c1636bfc196a34e0da3dde66
coffeedrunkpanda <52378774+coffeedrunkpanda@users.noreply.github.com>
2026-03-11 10:12:38 +0100

    chore: update graph to account for new rag routing

--- src/alpha_crunch/agent/graph.py
+++ src/alpha_crunch/agent/graph.py
@@ -1,6 +1,7 @@
-from langgraph.graph import StateGraph, END
+from langgraph.graph import StateGraph, START, END
 from alpha_crunch.agent.state import AgentState
 from alpha_crunch.agent.nodes import llm_node, intent_node, route_by_intent
+from alpha_crunch.agent.rag_node import rag_node
 
 def build_graph():
     
@@ -10,18 +11,20 @@ def build_graph():
     # Nodes
     graph.add_node("llm_node", llm_node)
     graph.add_node("intent_node", intent_node)
+    graph.add_node("rag_node", rag_node)
 
     # Entry point for the agent
-    graph.set_entry_point("intent_node")
+    graph.add_edge(START, "intent_node")
 
     # Intent node
     graph.add_conditional_edges(source="intent_node", # starting node
                                 path=route_by_intent, # next node
                                 path_map={ "llm_node": "llm_node",  # If router says "llm_node", go to "llm_node"
-                                          # "rag_node": "rag_node",  # TODO: Add rag system
+                                           "rag_node": "rag_node",
                                           })
     
     # Exit edge
+    graph.add_edge("rag_node", "llm_node")
     graph.add_edge("llm_node", END)
 
     print("🚨 IN BUILD GRAPH")
